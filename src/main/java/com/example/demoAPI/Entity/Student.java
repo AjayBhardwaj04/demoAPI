b7ff7d6eb9282f8a7b8a5c8fc8346d5616e74d9b
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-02-20 17:29:39 -0700

    entity file to student API

---
+++ src/main/java/com/example/demoAPI/Entity/Student.java
@@ -0,0 +1,27 @@
+package com.example.demoAPI.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import javax.annotation.processing.Generated;
+
+@Entity
+@Getter
+@Setter
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+
+
+
+}
